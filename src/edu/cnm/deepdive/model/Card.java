1dd0d69a6ee7d632ab94aaa8ca6ed02445cc8e45
Samuel Martinez <semartinez147@gmail.com>
2020-01-24 09:39:53 -0700

    Override Card.hashcode and Card.equals to check for duplicate cards.

--- src/edu/cnm/deepdive/model/Card.java
+++ src/edu/cnm/deepdive/model/Card.java
@@ -1,13 +1,17 @@
 package edu.cnm.deepdive.model;
 
+import java.util.Objects;
+
 public class Card {
 
   private final Suit suit;
   private final Rank rank;
+  private final int hash;
 
   public Card(Suit suit, Rank rank) {
     this.suit = suit;
     this.rank = rank;
+    hash = Objects.hash(suit, rank);
   }
 
   public Suit getSuit() {
@@ -21,5 +25,25 @@ public class Card {
   @Override
   public String toString() {
     return rank.symbol() + suit.symbol();
+  }
+
+  @Override
+  public int hashCode() {
+    return hash;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    boolean comparison = false;
+    if (obj == this) { // if the object we're testing points to the same heap object as this instance of Card
+      comparison = true;
+    } else if (obj instanceof Card // if the object is a (different) instance of Card
+        && obj.hashCode() == hash) { // and if the hashcodes match
+      Card other = (Card) obj; // "other" is an obj that we cast as type Card
+      if (suit == other.suit && rank == other.rank);
+      comparison = true;
+    }
+
+    return comparison;
   }
 }
